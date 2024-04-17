89c88b006f05c3893aaa427d27d7b188e9216ce9
Fraugue <167294493+Fraugue@users.noreply.github.com>
2024-04-17 09:38:54 +0000

    pas fini et ne fonctionne pas vraiment

---
+++ my_print_ascii.c
@@ -0,0 +1,12 @@
+int main(int argc, char const *argv[])
+{
+    
+int my_print_ascii(void);
+    my_print_ascii();
+
+    void print_char(char c);{
+       int write(int);
+       write(5);
+
+    return 0;
+    }}
