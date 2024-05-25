153d0e48ebb0a818a62231491643b6e4fd891684
Fraugue <167294493+Fraugue@users.noreply.github.com>
2024-05-25 12:19:02 +0000

    bcn

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -1,18 +1,23 @@
-int print_char(char);
-int Nbr;
-int AsciiNbr;
+int print_char (char);
 
-
-void my_print_n_ascii(void){
-
-    printf("how many: \n"); 
-    scanf("%d", &Nbr);
-
-    for(int i = 41+)
+void my_print_n_ascii(int howMany)
+{
+    int nb =  32 ;
+    if (howMany >= 95)
     {
-        print_char(i);
+        print_char(48);
     }
-
+        else
+    {
+        while (nb <= howMany + 31)
+        {
+            print_char(nb);
+            nb += 1;
+        }
+        
+    };
+    
 
     
-}
+    
+};
